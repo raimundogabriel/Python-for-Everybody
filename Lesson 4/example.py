c4079018217de5d89aa462b48b44f1a5561752fe
Gabriel Raimundo <raimundogabriel18@gmail.com>
2021-10-13 22:59:00 -0300

    Part 1 Chapter 3
    Starting with condition expressions.

---
+++ Lesson 4/example.py
@@ -0,0 +1,21 @@
+#exercises and examples of conditional Sentences
+x=5
+if x == 5:
+    print('Equal to 5')
+if x > 4:
+    print('Greater than 4')
+if x >= 5:
+    print('Greater than or Equals 5')
+if x < 6:
+    print('Less than 6')
+if x<=5:
+    print('Less than or Equals 5')
+if x != 6:
+    print('Not Equal 6')
+
+y = 4
+if y > 2:
+    print('Bigger')
+else:
+    print('Smaller')
+print('All done!')
